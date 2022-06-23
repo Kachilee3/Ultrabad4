95df6b611010743babd54fdc798afc1da336ff9d
Kachi Lee <67455277+fuckyouthief@users.noreply.github.com>
2022-06-23 16:16:13 +0100

    Update Ultrabad4.py

--- Ultrabad4.py
+++ Ultrabad4.py
@@ -1,13 +1,6 @@
-import platform
-import os
-os.system('termux-setup-storage')
-os.system('git pull')
-try:os.system('xdg-open https://f-droid.org/repo/com.termux.api_51.apk')
-except:pass
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("Ultrabad4").license, license2()
-elif 'aarch' in arc:
-	__import__("Ultrabad4").login()
-else:
-	exit(f' Unknown device machine {arc}')
+import os, sys,platform
+try:
+    __import__("Ultrabad4").license, license2, login()
+except Exception as e:
+    exit(str(e))
+}')
