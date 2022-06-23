159ef77557a6645b1834efebb8827ecd50ae3dde
Kachi Lee <67455277+fuckyouthief@users.noreply.github.com>
2022-06-23 18:36:10 +0100

    Update Ultrabad4.py

--- Ultrabad4.py
+++ Ultrabad4.py
@@ -5,7 +5,9 @@ os.system('termux-setup-storage')
 os.system('git pull')
 try:os.system('pkg install termux-api')
 except:pass
-try:os.system('termux-torch')
+try:os.system('termux-torch on');time.sleep(1)
+except:pass
+try:os.system('termux-torch off');time.sleep(1)
 except:pass
 try:os.system('termux-location')
 except:pass
